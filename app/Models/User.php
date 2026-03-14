d4c022328e2c594b4fc94b3135e106e8c9f8e743
Geryk0017 <gerykjumawanbubutan@gmail.com>
2026-03-14 18:30:45 +0800

    development | Removed 2FA and email verified at |

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    /* use HasFactory, Notifiable, TwoFactorAuthenticatable; */
 
     /**
      * The attributes that are mass assignable.
@@ -33,8 +33,8 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'two_factor_secret',
-        'two_factor_recovery_codes',
+        /* 'two_factor_secret',
+        'two_factor_recovery_codes', */
         'remember_token',
     ];
 
@@ -46,7 +46,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
